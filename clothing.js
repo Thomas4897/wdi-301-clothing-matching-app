1f4c89dd6a0dc9f4cc8f91bcf447616d4b5c3147
Thomas Maynard <90271276+Thomas4897@users.noreply.github.com>
2022-05-19 20:50:29 +0800

    Adding main.js code and css styling

--- clothing.js
+++ clothing.js
@@ -1,47 +1,3 @@
-// import { clothing } from "./clothing";
-// console.log("Clothing Array:", clothing);
-const currentDate = document.getElementById("CurrentDate");
-const topImg = document.getElementById("TopImg");
-const bottomImg = document.getElementById("BottomImg");
-const shoesImg = document.getElementById("ShoesImg");
-const tryAgainButton = document.getElementById("TryAgainButton");
-
-//! if you want the actual name of the weekday you can do this:
-const weekday = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-//! Determines the correct nth of given number
-const nth = function (d) {
-  if (d > 3 && d < 21) return "th";
-  switch (d % 10) {
-    case 1:
-      return "st";
-    case 2:
-      return "nd";
-    case 3:
-      return "rd";
-    default:
-      return "th";
-  }
-};
-
-const getCurrentDate = new Date();
-const currentDateArr = getCurrentDate.toDateString().split(" ");
-//! .getDay gives you a number from 0 to 6 where Sunday is 0 and Saturday is 6
-const getDayOfTheWeek = weekday[getCurrentDate.getDay()];
-currentDateArr[0] = getDayOfTheWeek;
-currentDateArr[2] = currentDateArr[2] + nth(currentDateArr[2]) + ",";
-const currentDateString = currentDateArr.join(" ");
-
-currentDate.innerHTML = `${currentDateString}`;
-
 const clothing = [
   {
     description: "Michael Kors Blue Dress Shirt",
@@ -128,13 +84,6 @@ const clothing = [
       "https://slimages.macysassets.com/is/image/MCY/products/6/optimized/21650286_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$&fmt=webp",
   },
   {
-    description: "Sperry Brown Shoes",
-    type: "shoes",
-    dressCode: "formal",
-    imageUrl:
-      "https://marvel-b1-cdn.bc0a.com/f00000000114841/www.florsheim.com/styleguide/resources/images/about/styleTips/Q27/27header.jpg",
-  },
-  {
     description: "Casual Pants",
     type: "bottom",
     dressCode: "casual",
@@ -185,55 +134,4 @@ const clothing = [
   },
 ];
 
-const getClothingType = function (day) {
-  switch (day) {
-    case 0:
-      return "sport";
-    case 6:
-      return "casual";
-    default:
-      return "formal";
-  }
-};
-
-const filteredTypeClothes = clothing.filter((e) => {
-  if (e.dressCode.toLowerCase() === getClothingType(getCurrentDate.getDay())) {
-    return e;
-  }
-  // if (e.dressCode.toLowerCase() === "casual") {
-  //   return e;
-  // }
-  // if (e.dressCode.toLowerCase() === "sport") {
-  //   return e;
-  // }
-});
-
-const filterdTops = filteredTypeClothes.filter((e) => {
-  if (e.type.toLowerCase() === "top") {
-    return e;
-  }
-});
-const filterdBottoms = filteredTypeClothes.filter((e) => {
-  if (e.type.toLowerCase() === "bottom") {
-    return e;
-  }
-});
-const filterdShoes = filteredTypeClothes.filter((e) => {
-  if (e.type.toLowerCase() === "shoes") {
-    return e;
-  }
-});
-
-const randomIndex = (arr) => {
-  return Math.floor(Math.random() * arr.length);
-};
-
-topImg.src = filterdTops[randomIndex(filterdTops)].imageUrl;
-bottomImg.src = filterdBottoms[randomIndex(filterdBottoms)].imageUrl;
-shoesImg.src = filterdShoes[randomIndex(filterdShoes)].imageUrl;
-
-tryAgainButton.onclick = function () {
-  topImg.src = filterdTops[randomIndex(filterdTops)].imageUrl;
-  bottomImg.src = filterdBottoms[randomIndex(filterdBottoms)].imageUrl;
-  shoesImg.src = filterdShoes[randomIndex(filterdShoes)].imageUrl;
-};
+export { clothing };
